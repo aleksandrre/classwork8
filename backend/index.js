f697ba989896563f4c499cc4e522d6662f941c81
chokss <alekochokheli.01@gmail.com>
2025-03-19 18:22:58 +0400

    add other functionalities

--- backend/index.js
+++ backend/index.js
@@ -4,23 +4,19 @@ import cors from "cors";
 const app = express();
 app.use(cors());
 app.use(express.json());
-const data = [
-  { name: "bmw", cylinder: 4, engine: "5.2" },
-  { name: "audi", cylinder: 8, engine: "4.2" },
-  { name: "golf", cylinder: 2, engine: "2.0" },
-];
+const data = [];
 
 app.get("/data", (req, res) => {
   res.json(data);
 });
 
 app.post("/addItem", (req, res) => {
-  const { name, cylinder, engine } = req.body;
-  const item = { name, cylinder, engine };
+  const { name, email, password } = req.body;
+
+  const item = { name, email, password };
 
   data.push(item);
-
-  res.json({ success: true });
+  res.status(200).json({ success: true });
 });
 
 app.listen(3001, () => {
--- frontend/main.js
+++ frontend/main.js
@@ -1,24 +1,43 @@
+const cards = document.getElementById("cards");
+
 const getData = async () => {
   const response = await fetch("http://localhost:3001/data");
   const data = await response.json();
-  console.log(data);
+  cards.innerHTML = "";
+  data.forEach((item) => {
+    const card = document.createElement("div");
+    card.innerHTML = `<h3>name:${item.name}</h3>
+        <p>email:${item.email}</p>
+        <p>password:${item.password}</p>`;
+    card.classList.add("card");
+    cards.appendChild(card);
+  });
 };
 getData();
 
+const btn = document.getElementById("btn");
 const addItem = async () => {
+  const name = document.getElementById("name");
+  const email = document.getElementById("email");
+  const password = document.getElementById("password");
+
   const response = await fetch("http://localhost:3001/addItem", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      name: "tesla",
-      cylinder: 2,
-      engine: "6.2",
+      name: name.value,
+      email: email.value,
+      password: password.value,
     }),
   });
   const data = await response.json();
   console.log(data);
+  getData();
+  name.value = "";
+  email.value = "";
+  password.value = "";
 };
 
-addItem();
+btn.addEventListener("click", addItem);
