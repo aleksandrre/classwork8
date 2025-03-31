9e0a2456735ef2f333d0adc044ff9d4997ef2003
aleksandre chokheli <alekochokheli.01@gmail.com>
2025-03-31 19:08:29 +0400

    add some

--- frontend/main.js
+++ frontend/main.js
@@ -1,24 +1,49 @@
 const getData = async () => {
+  console.log("getuser");
+
   const response = await fetch("http://localhost:3001/data");
   const data = await response.json();
-  console.log(data);
+  return data.data;
 };
-getData();
 
-const addItem = async () => {
-  const response = await fetch("http://localhost:3001/addItem", {
+const btn = document.getElementById("btn");
+const addUser = async () => {
+  const name = document.getElementById("name");
+  const email = document.getElementById("email");
+  const password = document.getElementById("password");
+  console.log("adduser");
+  const response = await fetch("http://localhost:3001/addUser", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      name: "tesla",
-      cylinder: 2,
-      engine: "6.2",
+      name: name.value,
+      email: email.value,
+      password: password.value,
     }),
   });
   const data = await response.json();
-  console.log(data);
+  return data;
 };
 
-addItem();
+const cards = document.getElementById("cards");
+
+const showCards = async () => {
+  await addUser();
+  const data = await getData();
+  cards.innerHTML = "";
+  data.forEach((user) => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.innerHTML = `
+    <h3>${user.name}</h3>
+    <p>Email: ${user.email}</p>
+    <p>Password: ${user.password}</p>
+    
+    `;
+    cards.appendChild(card);
+  });
+};
+
+btn.addEventListener("click", showCards);
