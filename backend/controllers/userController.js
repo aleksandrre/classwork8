a1ef901235efc29c7264ca2c72df230d15a4515d
chokss <alekochokheli.01@gmail.com>
2025-04-13 13:25:49 +0400

    add some changes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -31,3 +31,64 @@ export const register = async (req, res) => {
     res.status(500).json({ success: false, message: "სერვერის შეცდომა" });
   }
 };
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ success: false, message: "შეავსეთ ყველა ველი" });
+    }
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      return res
+        .status(400)
+        .json({ success: false, message: "მეილი  არასწორია" });
+    }
+    const isPasswordValid = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+    if (!isPasswordValid) {
+      return res
+        .status(400)
+        .json({ success: false, message: "პაროლი არასწორია" });
+    }
+
+    res
+      .status(200)
+      .json({ success: true, message: "თქვენ წარმატებით შეხვედით " });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ success: false, message: "სერვერის შეცდომა" });
+  }
+};
+
+export const getAllUsers = async (req, res) => {
+  try {
+    const AllUser = await User.find();
+
+    res.status(200).json({ success: true, data: AllUser });
+  } catch (err) {
+    res.status(500).json({ sucess: false, message: "სერვერის შეცდომა" });
+  }
+};
+
+export const getOneUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+   
+    if (!id) {
+      return res
+        .status(400)
+        .json({ success: false, message: "სახელი არ არის მოწოდებული" });
+    }
+    const user = await User.findById(id);
+    res.status(200).json({ success: true, user });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "სერვერის შეცდომა" });
+  }
+};
--- backend/index.js
+++ backend/index.js
@@ -3,61 +3,31 @@ import cors from "cors";
 import mongoose from "mongoose";
 import User from "./models/User.js";
 import bcrypt from "bcrypt";
-import { register } from "./controllers/userController.js";
+import {
+  register,
+  login,
+  getAllUsers,
+  getOneUser,
+} from "./controllers/userController.js";
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.post("/register", register);
 
-app.post("/login", async (req, res) => {
-  try {
-    const { email, password } = req.body;
+app.post("/login", login);
 
-    if (!email || !password) {
-      return res
-        .status(400)
-        .json({ success: false, message: "შეავსეთ ყველა ველი" });
-    }
-    const existingUser = await User.findOne({ email });
-    if (!existingUser) {
-      return res
-        .status(400)
-        .json({ success: false, message: "მეილი  არასწორია" });
-    }
-    const isPasswordValid = await bcrypt.compare(
-      password,
-      existingUser.password
-    );
-    if (!isPasswordValid) {
-      return res
-        .status(400)
-        .json({ success: false, message: "პაროლი არასწორია" });
-    }
-    
-    res
-      .status(200)
-      .json({ success: true, message: "თქვენ წარმატებით შეხვედით " });
-  } catch (error) {
-    console.log(error.message)
-    res.status(500).json({ success: false, message: "სერვერის შეცდომა" });
-  }
-});
+app.get("/getallUser", getAllUsers);
+console.log("s");
 
-app.get("/getallUser", async (req, res) => {
-  try {
-    const AllUser = await User.find();
-    res.status(200).json({ success: true, data: AllUser });
-  } catch (err) {
-    res.status(500).json({ sucess: false, message: "სერვერის შეცდომა" });
-  }
-});
+app.get("/getOneUser/:id", getOneUser);
 
 app.listen(3001, () => {
   console.log("სერვერი დაისტარტა 3001 პორტზე");
   mongoose
     .connect(
-      "mongodb+srv://tamarichokheli:12345@cluster0.0l5qo6f.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0"
+      "mongodb+srv://aleksandre:12345@cluster0.nx872oi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     )
     .then(() => console.log("ბაზასთან (mongodb) კავშირი წარმატებულია"))
     .catch((err) =>
